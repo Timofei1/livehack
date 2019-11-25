8ab097b4fc3ac23ea6ee58690c54e0b883181cc5
Ivan Melentev <i.melentev@schelcol.ru>
2019-11-25 21:58:00 +0300

    Рефакторинг 43й панели и удаление странных символов из index

--- src/index.js
+++ src/index.js
@@ -4,20 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vk-connect';
 import App from './App';
-<<<<<<< HEAD
 import registerServiceWorker from './sw';
-=======
->>>>>>> d249bf853685ae0946aad1ed5d9e6920448a30d3
 
 connect.send('VKWebAppInit');
-<<<<<<< HEAD
-
-// Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
-// расскомментируйте строку с registerServiceWorker();
-// Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
-// Подробнее про сервис воркеры можно почитать тут — https://vk.cc/8MHpmT
 registerServiceWorker();
-
-=======
->>>>>>> d249bf853685ae0946aad1ed5d9e6920448a30d3
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/panels/Persik43.js
+++ src/panels/Persik43.js
@@ -18,42 +18,58 @@ const osName = platform();
 const Persik43 = props => (
 	<Panel id={props.id}>
 		<PanelHeader
-			left={<HeaderButton onClick={props.go} data-to="home">
-				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-			</HeaderButton>}
-		>
+			left = {
+				<HeaderButton onClick={props.go} data-to="home">
+					{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+				</HeaderButton>
+			}>
 			Разработчики
 		</PanelHeader>
 		<Group title="">
-
+			<Div>
+				<a href="https://vk.com/rulon4ik_official">
+					<h align="left">
+						<Button size="xl" level="3" before={
+							<img src="https://sun9-38.userapi.com/c857528/v857528209/100a1b/DzjzNpcJ2wg.jpg"
+								 data-post-id="288498260_706"
+								 data-post-click-type="post_owner_img"
+								 width="50"
+								 height="50"
+								 class="post_img"/>
+						}>
+							Тимофей Рулонов
+							<div class="Cell__description">
+							Основатель
+							</div>
+						</Button>
+					</h>
+				</a>
+			</Div>
 		<Div>
-		<a href="https://vk.com/rulon4ik_official"><h align="left"><Button size="xl" level="3" before=
-		<img src="https://sun9-38.userapi.com/c857528/v857528209/100a1b/DzjzNpcJ2wg.jpg" data-post-id="288498260_706" data-post-click-type="post_owner_img" width="50" height="50" class="post_img" /> > Тимофей Рулонов 
-		<div class="Cell__description">Основатель</div>
-		</Button></h></a>
-		</Div>
-        
-		
-        
-        <Div>
-        <a href="https://vk.com/vladimir_vladimirowich_putin"><Button size="xl" level="3" before=
-		<img src="https://sun9-5.userapi.com/c857528/v857528583/fbdd9/ANBlOxOVvNE.jpg" data-post-id="288498260_706" data-post-click-type="post_owner_img" width="50" height="50" class="post_img" /> > Кириллл Малыгин 
-		<div class="Cell__description">Гл. модератор</div>
-		</Button>
+			<a href="https://vk.com/vladimir_vladimirowich_putin">
+				<Button size="xl" level="3" before={
+					<img
+						src="https://sun9-5.userapi.com/c857528/v857528583/fbdd9/ANBlOxOVvNE.jpg"
+						data-post-id="288498260_706"
+						data-post-click-type="post_owner_img"
+						width="50"
+						height="50"
+						class="post_img"/>
+				}>
+					Кириллл Малыгин
+					<div class="Cell__description">Гл. модератор</div>
+				</Button>
 		</a>
-        </Div>
-        </Group>
-        
+	</Div>
+	</Group>
         <Group>
-        <center>
-        
-        
-        
-        <br/>
-        <h>Вы можете помочь приложению </h><a href="https://vk.com/topic-189105798_40593021">предложив свои лайфхаки</a>
-        <br/>
-        <br/>
-        </center>
+			<center>
+				<br/>
+				<h>Вы можете помочь приложению </h>
+				<a href="https://vk.com/topic-189105798_40593021">предложив свои лайфхаки</a>
+				<br/>
+				<br/>
+			</center>
         </Group>
     </Panel>
 );
