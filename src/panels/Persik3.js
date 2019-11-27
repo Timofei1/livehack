f914fb2c3b30d5f98059fb8583eff43dd21c4e9e
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2019-11-27 13:51:53 +0400

    live

--- src/panels/Persik3.js
+++ src/panels/Persik3.js
@@ -47,6 +47,13 @@ const Persik3 = props => (
 
             </Div>
         </Group>
+        <Group>
+            <Div>
+            <center>
+                <a href="https://vk.com/topic-189105798_40599476">Открыть комментарии</a>
+            </center>
+            </Div>
+        </Group>
     </Panel>
 );
 
