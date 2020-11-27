96ca9ddda647689075b4fdd01a02da1f66fb2e9d
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2020-11-27 16:19:25 +0400

    Updates

--- precache-manifest.aeeacc2bf5092000f8700aa8c93ffa31.js
+++ precache-manifest.aeeacc2bf5092000f8700aa8c93ffa31.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94e31157b9c358cc07af376ec51130c8",
+    "revision": "33ef21eaecf7638e2b88c370c9885b6d",
     "url": "/livehack/index.html"
   },
   {
-    "revision": "4ba5335e78b44c97a87d",
+    "revision": "c02268c2983b5a94e6a1",
     "url": "/livehack/static/css/2.2cfa1b23.chunk.css"
   },
   {
-    "revision": "fabe50fba4751880ba92",
+    "revision": "bdfd516ec5887517880e",
     "url": "/livehack/static/css/main.159ac38d.chunk.css"
   },
   {
-    "revision": "4ba5335e78b44c97a87d",
-    "url": "/livehack/static/js/2.9e44352d.chunk.js"
+    "revision": "c02268c2983b5a94e6a1",
+    "url": "/livehack/static/js/2.deda2f4c.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/livehack/static/js/2.9e44352d.chunk.js.LICENSE.txt"
+    "url": "/livehack/static/js/2.deda2f4c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fabe50fba4751880ba92",
-    "url": "/livehack/static/js/main.6eaebefd.chunk.js"
+    "revision": "bdfd516ec5887517880e",
+    "url": "/livehack/static/js/main.b64c2a97.chunk.js"
   },
   {
     "revision": "6b31b5c0f540ae4c1e4c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/livehack/precache-manifest.42ffc1ee7f4459c1fe532bbb174dec83.js"
+  "/livehack/precache-manifest.aeeacc2bf5092000f8700aa8c93ffa31.js"
 );
 
 self.addEventListener('message', (event) => {
